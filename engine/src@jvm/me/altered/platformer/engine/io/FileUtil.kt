e38ac67c04748b6920956cc51c729124c75f5a5c
altered <altered_games@outlook.com>
2024-09-21 14:58:21 +0500

    update to skiko 0.8.14

--- engine/src@jvm/me/altered/platformer/engine/io/FileUtil.kt
+++ engine/src@jvm/me/altered/platformer/engine/io/FileUtil.kt
@@ -2,7 +2,6 @@ package me.altered.platformer.engine.io
 
 import org.jetbrains.skia.Font
 import org.jetbrains.skia.Typeface
-import org.jetbrains.skia.makeFromFile
 
 actual fun Any.resource(path: String): String? {
     val path = if (path.startsWith('/')) path else "/$path"
@@ -11,5 +10,5 @@ actual fun Any.resource(path: String): String? {
 
 actual fun font(path: String?, size: Float): Font {
     val path = path?.substringAfter('/')
-    return Font(null?.let { Typeface.makeFromFile(it) }, size)
+    return Font(null?.let { Typeface.makeEmpty() }, size)
 }
