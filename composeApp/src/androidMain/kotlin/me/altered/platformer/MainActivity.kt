010a22ad5f22459da9a0e2d1778e9b2438bea82b
altered <altered_games@outlook.com>
2024-12-21 13:10:27 +0500

    PLATFORMER-2: fix fullscreen on android

--- composeApp/src/androidMain/kotlin/me/altered/platformer/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/me/altered/platformer/MainActivity.kt
@@ -1,6 +1,9 @@
 package me.altered.platformer
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import me.altered.platformer.engine.logger.AndroidLogger
@@ -10,6 +13,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Logger.defaultLogger = AndroidLogger()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.systemBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
         setContent { App() }
     }
 }
